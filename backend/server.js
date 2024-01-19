7dc3907cd64e0b5dfbc8e3be773b68621a5a28cd
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2024-01-19 19:47:38 +0530

    pushing user data into db

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,52 @@
-const mongoose= require('mongoose')
-const express= require('express')
+const mongoose = require("mongoose");
+const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-const fs=require('fs');
+const fs = require("fs");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const User = require("./model/schema");
+const cors = require("cors");
 
-const app=express();
+const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-const schema= new mongoose.Schema({
-    user_id:Number,
-    password: String,
-    name: String
-})
+const connectionString = process.env.MONGO_URI;
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to the database…"))
+  .catch((err) => console.error("Connection error:", err));
 
+app.get("/", (req, res) => {
+  res.send("home page");
+});
 
-const connectionString = process.env.MONGO_URI; 
-mongoose.connect(connectionString)
-.then(() => console.log('Connected to the database…'))
-.catch((err) => console.error('Connection error:', err));
+app.get("/user", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/html/indexuser.html"));
+});
 
-app.get('/',(req,res)=>{
-    res.send('home page')
-})
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/html/indexadmin.html"));
+});
 
-app.get('/user', (req, res) => {
-    // Serve user.html for /user route
-    res.sendFile(path.join(__dirname, '../frontend/indexuser.html'));
-  });
-  
-  app.get('/admin', (req, res) => {
-    // Serve admin.html for /admin route
-    res.sendFile(path.join(__dirname, '../frontend/indexadmin.html'));
-  });
+app.post("/submitForm", async (req, res) => {
+  const { userid, password } = req.body;
 
-app.listen(3000,()=>{
-    console.log("server is listening");
-})
+  const newUser = new User({ userid, password });
+
+  console.log(userid);
+  console.log(password);
+
+  try {
+    // Saving the user document to MongoDB
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    console.error("Error saving user to the database:", error);
+    res.status(500).json({ error: "Error saving user to the database." });
+  }
+});
+app.listen(3000, () => {
+  console.log("server is listening");
+});
